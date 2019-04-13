cf07499f6fab02c04b63db2cf3966bc5f6e86356
hamedmirlohi <hamed0311@gmail.com>
2019-04-13 16:58:23 -0700

    succesfully reading from a file

--- homework1.py
+++ homework1.py
@@ -1,1 +1,28 @@
-print("hi")
+import re
+
+
+# open CPU dump in Read Only mode
+pFile = open("silicon_data.txt","r")
+
+while True:
+    # read a single line
+    line = pFile.readline()
+    # end of the file? exit loop
+    if not line:
+        break
+    print(line)
+
+pFile.close()
+
+
+'''
+sent = "1  0.9	1900                       1,1"
+
+#matchObj = re.split("", sent)
+
+saved = sent.split()
+#print(saved[3])
+
+for x in saved:
+    print(x)
+'''
