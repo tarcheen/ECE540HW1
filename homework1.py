dec1008c8fe21ea7e2d15348f6a47c5de669899c
hamedmirlohi <hamed0311@gmail.com>
2019-04-13 21:47:21 -0700

    parse from file and insert values to voltage, frequency and Die baskets

--- homework1.py
+++ homework1.py
@@ -1,8 +1,15 @@
 import re
 
+VOLTAGE = 1
+FREQUENCY = 2
+DIE_LOCATION = 3
 
 # open CPU dump in Read Only mode
 pFile = open("silicon_data.txt","r")
+
+voltage = []
+freq    = []
+dieLoc  = []
 
 while True:
     # read a single line
@@ -10,19 +17,16 @@ while True:
     # end of the file? exit loop
     if not line:
         break
-    print(line)
+
+    # if blank line detected or word "id" detected, SKIP
+    if (not line.strip() or "id" in line):
+        continue
+
+    match = line.split()
+
+    # insert values to appropriate lists
+    voltage.append(match[VOLTAGE])
+    freq.append(match[FREQUENCY])
+    dieLoc.append(match[DIE_LOCATION])
 
 pFile.close()
-
-
-'''
-sent = "1  0.9	1900                       1,1"
-
-#matchObj = re.split("", sent)
-
-saved = sent.split()
-#print(saved[3])
-
-for x in saved:
-    print(x)
-'''
