7cd3d2992729db9706630efdc4e5c5b1489c1a20
hamedmirlohi <hamed0311@gmail.com>
2019-04-14 23:46:42 -0700

    succesfully writing output to another file and plots are displayed correctly

--- homework1.py
+++ homework1.py
@@ -1,15 +1,19 @@
 import re
+import matplotlib.pyplot as plt
+from operator import itemgetter
 
-VOLTAGE = 1
-FREQUENCY = 2
-DIE_LOCATION = 3
+#VOLTAGE = 1
+#FREQUENCY = 2
+#DIE_LOCATION = 3
 
 # open CPU dump in Read Only mode
 pFile = open("silicon_data.txt","r")
 
-voltage = []
-freq    = []
-dieLoc  = []
+#voltage = []
+#freq    = []
+#dieLoc  = []
+
+parsedList = []
 
 while True:
     # read a single line
@@ -22,11 +26,47 @@ while True:
     if (not line.strip() or "id" in line):
         continue
 
+    # split all values by whitespace
     match = line.split()
 
+    # split die locations by comma
+    locations = match[3].split(",")
+    # die locations saved, pop the last element which includes the location
+    match.pop()
+    # pop the first element as well, numbers not needed
+    match.pop(0)
+    # insert xLocation to list
+    match.append(locations[0])
+    # insert yLocation to list
+    match.append(locations[1])
+    # convert all elements to integer
+    #match = [ float(x) for x in match]
+
+    parsedList.append(match)
     # insert values to appropriate lists
-    voltage.append(match[VOLTAGE])
-    freq.append(match[FREQUENCY])
-    dieLoc.append(match[DIE_LOCATION])
+    #voltage.append(match[VOLTAGE])
+    #freq.append(match[FREQUENCY])
+    #dieLoc.append(match[DIE_LOCATION])
+
 
 pFile.close()
+
+#sort list elements in respect to y-values
+sortedParsedList = sorted(parsedList, key=itemgetter(3))
+
+# all contents are read, now lets write everything to new file
+pFile = open("sorted_data.txt",'w')
+
+#write the titles
+pFile.write("#id\tvoltage(V)\tfrequency(Mhz)\tDieLocation(x,y)\n\n")
+
+#write all sorted values to file, plot all the values
+for counter, x in enumerate(sortedParsedList):
+    pFile.write(str(counter) + "\t\t" + str(x[0]) + "\t\t" + str(x[1]) + "\t\t" + str(x[2])+","+ str(x[3]) + "\n")
+    plt.scatter(x[0],x[1])
+
+# done, close the file
+pFile.close()
+
+#display the plots
+plt.show()
