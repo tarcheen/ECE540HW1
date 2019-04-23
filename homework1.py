6630de3cfd471794dd758623dedaae3500b64c3e
hamedmirlohi <hamed0311@gmail.com>
2019-04-22 21:48:15 -0700

    Added feature to sort by X-Values first, then sort by Y-Values, more code clean up

--- homework1.py
+++ homework1.py
@@ -3,7 +3,7 @@ Hamed Mirlohi
 ECE 510 Post-Silicon Validation
 Homework #1
 
-github: 
+github: https://github.com/tarcheen/ECE540HW1
 '''
 
 import matplotlib.pyplot as plt
@@ -18,6 +18,7 @@ X_LOCATION = 2
 Y_LOCATION = 3
 DIE_X_Y = 3
 
+
 def process_Line(line):
     # split all values by whitespace
     match = line.split()
@@ -26,49 +27,45 @@ def process_Line(line):
     locations = match[DIE_X_Y].split(",")
     # die locations saved, pop the last element which includes the location
     match.pop()
-    # pop the first element as well, numbers not needed
+    # pop the first element as well, enumerated numbers not needed
     match.pop(INDEX)
     # insert xLocation to list
     match.append(locations[X_VALUE])
     # insert yLocation to list
     match.append(locations[Y_VALUE])
-    
+
     return match
 
 
 # open CPU dump in Read Only mode
 with open("silicon_data.txt", "r") as pFile:
-
     parsedList = []
 
     while True:
         # read a single line
         line = pFile.readline()
-        # end of the file? exit loop
+        # end of the file? if yes, exit the while loop
         if not line:
             break
 
-    # if blank line detected or word "id" detected, SKIP
+        # if blank line detected or word "id" detected, SKIP
         if (not line.strip() or "id" in line):
             continue
-
+        # storing every lines element as a list
         parsedList.append(process_Line(line))
 
-# sort list elements in respect to y-values
-sortedParsedList = sorted(parsedList, key=itemgetter(Y_LOCATION))
+# sort list elements in respect to x-values first, then y-values
+sortedParsedList = sorted(parsedList, key=itemgetter(X_LOCATION, Y_LOCATION))
 
 # all contents are read, now lets write everything to new file
-#pFile = open("sorted_data.txt", 'w')
-
 with open("sorted_data.txt", 'w') as sFile:
-
     # write the titles
     sFile.write("#id\tvoltage(V)\tfrequency(Mhz)\tDieLocation(x,y)\n\n")
 
     # write all sorted values to file, plot all the values
     for counter, x in enumerate(sortedParsedList):
         sFile.write(str(counter + 1) + "\t\t" + str(x[VOLTAGE]) + "\t\t" + str(x[FREQUENCY]) + "\t\t" + str(
-        x[X_LOCATION]) + "," + str(x[Y_LOCATION]) + "\n")
+            x[X_LOCATION]) + "," + str(x[Y_LOCATION]) + "\n")
         # plot voltage and frequency
         plt.scatter(x[VOLTAGE], x[FREQUENCY])
 
